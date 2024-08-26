2e53fc0bd2a2bc77052de1a5e9cc4d19111bdecd
Ya-Danya <ddsamsonenko@edu.hse.ru>
2024-08-26 14:32:52 +0300

    Initial commit

---
+++ authenticationService/src/main/java/ru/gymbros/authentificationService/AuthentificationServiceApplication.java
@@ -0,0 +1,13 @@
+package ru.gymbros.authentificationService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthentificationServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthentificationServiceApplication.class, args);
+	}
+
+}
